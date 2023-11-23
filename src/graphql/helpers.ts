f0f9c7624a2157d874698912d9a4c6deb122d9dd
Kamil Kisiela <kamil.kisiela@gmail.com>
2023-11-23 21:17:51 +0100

    Make stripFederationFromSupergraph less strict
    Remove only federation-related directives

--- src/graphql/helpers.ts
+++ src/graphql/helpers.ts
@@ -1,19 +1,51 @@
-import { DefinitionNode, DirectiveDefinitionNode, DocumentNode, Kind, visit } from 'graphql';
+import {
+  DefinitionNode,
+  DirectiveDefinitionNode,
+  DocumentNode,
+  Kind,
+  specifiedDirectives,
+  visit,
+} from 'graphql';
 
 export function isDirectiveDefinition(node: DefinitionNode): node is DirectiveDefinitionNode {
   return node.kind === Kind.DIRECTIVE_DEFINITION;
 }
 
 export function stripFederationFromSupergraph(supergraph: DocumentNode) {
-  function remove() {
-    return null;
+  function removeDirective(node: {
+    name: {
+      value: string;
+    };
+  }) {
+    const directiveName = node.name.value;
+    const isSpecifiedDirective = specifiedDirectives.some(d => d.name === directiveName);
+    if (!isSpecifiedDirective) {
+      const isFederationDirective =
+        directiveName === 'link' ||
+        directiveName === 'inaccessible' ||
+        directiveName === 'tag' ||
+        directiveName === 'join__graph' ||
+        directiveName === 'join__type' ||
+        directiveName === 'join__implements' ||
+        directiveName === 'join__unionMember' ||
+        directiveName === 'join__enumValue' ||
+        directiveName === 'join__field';
+
+      if (isFederationDirective) {
+        return null;
+      }
+    }
   }
 
   return visit(supergraph, {
-    DirectiveDefinition: remove,
-    Directive: remove,
-    SchemaDefinition: remove,
-    SchemaExtension: remove,
+    DirectiveDefinition: removeDirective,
+    Directive: removeDirective,
+    SchemaDefinition() {
+      return null;
+    },
+    SchemaExtension() {
+      return null;
+    },
     EnumTypeDefinition: node => {
       if (
         node.name.value === 'core__Purpose' ||
