443283e22e89934a268b7a6318c02ffc3bfbf464
Laurin Quast <laurinquast@googlemail.com>
2023-11-24 16:29:40 +0100

    feat: public schema from supergraph (#21)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './compose.js';
 export * from './types.js';
 export * from './validate.js';
-export { stripFederationFromSupergraph } from './graphql/helpers.js';
+export { transformSupergraphToPublicSchema } from './graphql/transform-supergraph-to-public-schema.js';
