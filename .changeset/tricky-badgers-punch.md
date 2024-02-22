de983b02479bdb4aee80bd42f6faece62586a45f
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-02-22 14:23:04 +0100

    Add containsSupergraphSpec (#44)
    This method is meant to quickly check if
    `transformSupergraphToPublicSchema` is needed.
    Obviously, in most cases when `transformSupergraphToPublicSchema` is
    used, a provided schema contains Supergraph spec (join__Graph etc).
    
    Why do I create it then?
    We have a special case in GraphQL Hive where we persist the output of
    `transformSupergraphToPublicSchema`, but when Apollo Federation adds
    something new, we might want to run this method again to remove new
    pieces.
    
    #### Performance
    
    I used a Supergraph SDL with ~20k LOC and create three copies.
    First copy used `field(whatever: join__Graph)` as an argument somewhere
    in the middle.
    Second copy had `scalar join__DirectiveArguments`, also in the middle of
    the file.
    Third copy had `directive @join__directive` definition (yeah yeah, in
    the middle).
    
    I wrote three versions of `containsSupergraphSpec` to make sure it has
    minimal performance footprint.
    
    1. for-loop over all federation scalars, enums and directives that used
    `sdl.includes("[name") or sdl.includes(" name")`
    2. same for-loop but directives where checked first
    3. regex (current implementation).
    
    I ran it 1000 times and I got (average):
    1. 1.56 ms
    2. 0.83 ms
    3. 0.59 ms

---
+++ .changeset/tricky-badgers-punch.md
@@ -0,0 +1,5 @@
+---
+'@theguild/federation-composition': patch
+---
+
+Add containsSupergraphSpec to detect if Supergraph related scalars, enums or directives are used
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from './compose.js';
 export * from './types.js';
 export * from './validate.js';
 export { transformSupergraphToPublicSchema } from './graphql/transform-supergraph-to-public-schema.js';
+export { containsSupergraphSpec } from './graphql/contains-supergraph-spec.js';
 export { sortSDL } from './graphql/sort-sdl.js';
